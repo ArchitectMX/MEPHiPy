f8a72806fe343dbfb42117ee60b00f9c0f4f122d
Roman Berendeev <romanberendeev-mac@Romans-MacBook-Pro.local>
2026-09-13 00:05:49 +0300

    task 7

--- TasksFrom08_09_2026/task7.py
+++ TasksFrom08_09_2026/task7.py
@@ -1,6 +1,38 @@
-with open("task7.txt") as f:
+from faker import Faker
+
+
+top_words: dict[str, int] = {}
+
+
+with open("TasksFrom08_09_2026/task7.txt") as f:
     for i in f:
-        for symb in ".,!?:;":
-            i.replace(symb, "")
-        words = i.split()
-        print(words)
+        for symb in ",!?:;.":
+            i = i.replace(symb, "")
+        words = i.lower().split()
+        for word in words:
+            if word in top_words.keys():
+                top_words[word] += 1
+            else:
+                top_words[word] = 1
+
+
+def top_10(top_words):
+    return sorted(top_words.items(), key=lambda x: x[1], reverse=True)[:10]
+
+
+top = top_10(top_words)
+
+for word, count in top:
+    print(word, count)
+
+def create_10k_words():
+    fake = Faker("ru_RU")
+
+    with open("task7_faker.txt", "w") as f:
+        for _ in range(10000):
+            words = f"{fake.name()} живет в городе {fake.city()} и сегодня {fake.word()} {fake.word()} {fake.word()}."
+
+            f.write(words + "\n")
+
+
+create_10k_words()

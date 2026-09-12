7fa7403cc951e8b1cc9ccceb1dc1c4c9b1db6cfa
Roman Berendeev <romanberendeev-mac@Romans-MacBook-Pro.local>
2026-09-13 02:55:09 +0300

    Demo accounts: done

--- TasksFrom08_09_2026/task9.py
+++ TasksFrom08_09_2026/task9.py
@@ -13,20 +13,20 @@ class BankAccount:
 
     def deposit(self, money):
         if money <= 0:
-            self._log.append(f"{datetime.now().strftime('%d.%m.%Y %H:%M:%S')} deposit failed: invalid amount {money}")
-            raise ValueError("amount must be positive")
+            self._log.append(f"{datetime.now().strftime('%d.%m.%Y %H:%M:%S')} deposit failed: invalid amount: {money}")
+            return ValueError("amount must be positive")
 
         self._balance += money
         self._log.append(f"{datetime.now().strftime('%d.%m.%Y %H:%M:%S')} deposit: {money}")
 
     def withdraw(self, money):
         if money <= 0:
-            self._log.append(f"{datetime.now().strftime('%d.%m.%Y %H:%M:%S')} withdrawal failed: invalid amount {money}")
-            raise ValueError("amount must be positive")
+            self._log.append(f"{datetime.now().strftime('%d.%m.%Y %H:%M:%S')} withdrawal failed: invalid amount: {money}")
+            return ValueError("amount must be positive")
 
         if money > self._balance:
-            self._log.append(f"{datetime.now().strftime('%d.%m.%Y %H:%M:%S')} withdrawal failed: insufficient funds ({money})")
-            raise ValueError("insufficient funds")
+            self._log.append(f"{datetime.now().strftime('%d.%m.%Y %H:%M:%S')} withdrawal failed: insufficient funds: {money}")
+            return ValueError("insufficient funds")
 
         self._balance -= money
         self._log.append(f"{datetime.now().strftime('%d.%m.%Y %H:%M:%S')} withdrawal: {money}")
@@ -38,9 +38,9 @@ class BankAccount:
         return self._log
 
 
-class SavingAccount(BankAccount):
-    def __init__(self, user, log):
-        super().__init__(user, log)
+class SavingsAccount(BankAccount):
+    def __init__(self, user):
+        super().__init__(user)
         self._savingLimit = 1000
 
     @property
@@ -49,20 +49,20 @@ class SavingAccount(BankAccount):
 
     def withdraw(self, money):
         if money <= 0:
-            self._log.append(f"{datetime.now().strftime('%d.%m.%Y %H:%M:%S')} withdrawal failed: invalid amount {money}")
-            raise ValueError("amount must be positive")
+            self._log.append(f"{datetime.now().strftime('%d.%m.%Y %H:%M:%S')} withdrawal failed: invalid amount: {money}")
+            return ValueError("amount must be positive")
 
         if money + self._savingLimit > self._balance:
-            self._log.append(f"{datetime.now().strftime('%d.%m.%Y %H:%M:%S')} withdrawal failed: insufficient funds ({money})")
-            raise ValueError("insufficient funds")
+            self._log.append(f"{datetime.now().strftime('%d.%m.%Y %H:%M:%S')} withdrawal failed: insufficient funds: {money}")
+            return ValueError("insufficient funds")
 
         self._balance -= money
         self._log.append(f"{datetime.now().strftime('%d.%m.%Y %H:%M:%S')} withdrawal: {money}")
 
 
 class CreditAccount(BankAccount):
-    def __init__(self, user, log):
-        super().__init__(user, log)
+    def __init__(self, user):
+        super().__init__(user)
         self._creditLimit = 10000
 
     @property
@@ -71,23 +71,31 @@ class CreditAccount(BankAccount):
 
     def withdraw(self, money):
         if money <= 0:
-            self._log.append(f"{datetime.now().strftime('%d.%m.%Y %H:%M:%S')} withdrawal failed: invalid amount {money}")
-            raise ValueError("amount must be positive")
+            self._log.append(f"{datetime.now().strftime('%d.%m.%Y %H:%M:%S')} withdrawal failed: invalid amount: {money}")
+            return ValueError("amount must be positive")
 
         if money - self._creditLimit > self._balance:
-            self._log.append(f"{datetime.now().strftime('%d.%m.%Y %H:%M:%S')} withdrawal failed: insufficient funds ({money})")
-            raise ValueError("insufficient funds")
+            self._log.append(f"{datetime.now().strftime('%d.%m.%Y %H:%M:%S')} withdrawal failed: insufficient funds: {money}")
+            return ValueError("insufficient funds")
 
         self._balance -= money
         self._log.append(f"{datetime.now().strftime('%d.%m.%Y %H:%M:%S')} withdrawal: {money}")
 
 
-newAcc = BankAccount("Name")
-newAcc.deposit(1000)
-newAcc.deposit(-100)
-newAcc.withdraw(900)
-newAcc.withdraw(900)
-newAcc.withdraw(-900)
-print(newAcc.get_balance())
-print(newAcc.get_history())
+accounts = [
+    BankAccount("Base"),
+    SavingsAccount("Saving"),
+    CreditAccount("Credit")
+]
 
+for account in accounts:
+    account.deposit(5000)
+
+for account in accounts:
+    account.withdraw(6000)
+
+for account in accounts:
+    print(type(account).__name__)
+    print("balance:", account.get_balance())
+    print("history:", account.get_history())
+    print()
